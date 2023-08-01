2cd0392f440522e7cd6b05218be9624532204dc7
Anthony Salemo <ajssalemo@gmail.com>
2023-08-01 19:06:44 -0400

    using the flags package for cli args

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"flag"
 )
 
 func main() {
 	fmt.Println("gutils")
+	commitMessage := flag.String("commit", "initial commit", "A commit message - defaults to 'initial commit'")
+	gitRemote := flag.String("remote", "origin", "The specified upstream git remote to push to - defaults to origin")
+	gitBranch := flag.String("branch", "main", "The branch to push to - defaults to main")
+	// flags package expects pointers back to the original values
+	fmt.Println(*commitMessage)
+	fmt.Println(*gitRemote)
+	fmt.Println(*gitBranch)
 }
