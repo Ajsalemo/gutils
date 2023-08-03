61bc1fc8b000224ded3b0734da5d7cddd58efeec
Anthony Salemo <ajssalemo@gmail.com>
2023-08-03 18:58:48 -0400

    test

--- main.go
+++ main.go
@@ -8,9 +8,13 @@ import (
 
 func main() {
 	fmt.Println("gutils")
-	commitMessage := flag.String("commit", "initial commit", "A commit message - defaults to 'initial commit'")
-	gitRemote := flag.String("remote", "origin", "The specified upstream git remote to push to - defaults to origin")
-	gitBranch := flag.String("branch", "main", "The branch to push to - defaults to main")
+	// Define a flag for the commit message to git
+	commitMessage := flag.String("c", "initial commit", "A commit message - defaults to 'initial commit'")
+	// Define a flag for the git remote
+	gitRemote := flag.String("r", "origin", "The specified upstream git remote to push to - defaults to origin")
+	// Define a flag for the branch to be pushed to
+	gitBranch := flag.String("b", "main", "The branch to push to - defaults to main")
+	// Parse all input
 	flag.Parse()
 
 	// flags package expects pointers back to the original values
