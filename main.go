e33c19e602d16eb77a0764091fcfb6e94f7f8029
Anthony Salemo <ajssalemo@gmail.com>
2023-08-26 19:24:33 -0400

    corrected 'nothing to push' message handling

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func executeGitAdd() {
 func executeGitCommit(commitMessage string) {
 	// git commit -m [commit message]
 	out, err := exec.Command("git", "commit", "-m", commitMessage).CombinedOutput()
-
+	fmt.Println(string(out))
 	// Check if we push a commit where nothing is going to be commited to the branch
 	// This is returned as an error - but we want to clean up the output of this and present this in less of a critical manner
 	// Therefor we return from those messages as exit(0) - which is fine and mimics normal Git behavior
